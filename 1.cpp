3def7bb180dc66a7eca383f6f950690be1f924ee
Sanskruti Shimple <84955646+Sanskruti-Shimple@users.noreply.github.com>
2023-12-09 20:27:03 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	string food1 = "pizza";
+	string food2 = "pasta";
+	string food3 = "Burger";
+	
+	cout << food1 << food2 << food3 << endl;
+	
+	return 0;
+	
+}
---
+++ 2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "size of char:- " << sizeof(char) << endl;
+	cout << "Size of int:- " << sizeof(int) << endl;
+	
+	cout << "Size of float:- " << sizeof(float) << endl;
+	cout << "Size of boolean:- " << sizeof(bool) << endl;
+	
+	cout << "Sizs of double:- " << sizeof(double) << endl;
+	cout << "Size of long:- " << sizeof(long) << endl;
+}
